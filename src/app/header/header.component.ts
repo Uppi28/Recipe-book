6d7dd157dfdff7b54e77c481a6da29fda4d4126c
uppi28 <upendrabr.93@gmail.com>
2019-06-29 18:01:11 +0530

    Shopping list and recipe routing done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { compileNgModule } from '@angular/compiler';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +10,12 @@ export class HeaderComponent implements OnInit {
 
   collapsed = true;
    
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature)
+  }
+
   constructor() { }
 
   ngOnInit() {
